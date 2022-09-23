54740fc8c17e8194fe2a41de388aab4b2408dff8
calebhadley1 <caleb@hadleynet.org>
2022-09-22 21:13:09 -0400

    Initial commit

---
+++ src/main/java/com/hadleynet/ExpenseTrackerV2/ExpenseTrackerV2Application.java
@@ -0,0 +1,13 @@
+package com.hadleynet.ExpenseTrackerV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hadleynet/ExpenseTrackerV2/ExpenseTrackerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadleynet.ExpenseTrackerV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
