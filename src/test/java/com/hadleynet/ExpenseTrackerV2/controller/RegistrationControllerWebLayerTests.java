fedc33dadb40cb3bc0e19c13395d8f7c7be69359
calebhadley1 <caleb@hadleynet.org>
2022-11-04 13:48:19 -0400

    Web layer tests for registration

---
+++ src/test/java/com/hadleynet/ExpenseTrackerV2/controller/RegistrationControllerWebLayerTests.java
@@ -0,0 +1,47 @@
+package com.hadleynet.ExpenseTrackerV2.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hadleynet.ExpenseTrackerV2.model.RegistrationRequest;
+import com.hadleynet.ExpenseTrackerV2.repository.AppUserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RegistrationControllerWebLayerTests {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @AfterEach
+    public void teardown() {
+        //Delete User
+        appUserRepository.deleteAll();
+    }
+
+    @Test
+    void testRegister() throws Exception {
+        RegistrationRequest registrationRequest = new RegistrationRequest(
+                "fname", "lname", "test@gmail.com", "password"
+        );
+        this.mvc.perform(post("/api/register")
+                        .content(objectMapper.writeValueAsString(registrationRequest))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andReturn();
+    }
+}
